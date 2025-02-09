4d3d840b607009be063920b2c4fd7e15c0602420
John Rogee Turqueza <jrogeet@gmail.com>
2025-02-09 22:51:16 +0800

    works and functions just right, tho the calculation's a bit off & can be optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Bill bill={bill} setBill={setBill}/>
+      <Service setTip={setTip} />
+      <ServiceFriend setFriendTip={setFriendTip}/>
+      <Total bill={bill} tip={tip} friendTip={friendTip}/>
     </>
   )
 }
 
-export default App
+function Bill({ bill, setBill }) {
+  return (
+    <>
+      <p>How much was the bill?</p>
+      <input type="number" onChange={(e) => setBill(Number(e.target.value))}/>
+    </>
+  );
+}
+
+function Service({ setTip }) {
+  return (
+    <>
+      <p>How did you like the service?</p>
+      <select onChange={(e) => setTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolute amazing! (20%)</option>
+      </select>
+    </>
+  );
+}
+
+function ServiceFriend({ setFriendTip }) {
+  return (
+    <>
+      <p>How did your friend like the service?</p>
+      <select onChange={(e) => setFriendTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolute amazing! (20%)</option>
+      </select>
+    </>
+  );
+}
+
+function Total({ bill, tip, friendTip }) {
+  const totalTip = (tip / 100) + (friendTip / 100) / 2;
+  const average = Math.round(totalTip * bill);
+  const total = bill + average;
+  console.log(totalTip);
+
+  return (
+    <>
+      <p>You pay {total} (${bill} + ${average} tip)</p>
+      <button>Reset</button>
+    </>
+  );
+}
